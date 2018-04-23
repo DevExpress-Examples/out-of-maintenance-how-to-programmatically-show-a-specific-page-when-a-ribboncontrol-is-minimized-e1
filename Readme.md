12afb17167c78eb96f6bb4054b3b339c19305d83
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ Readme.md
@@ -0,0 +1,8 @@
+# How to programmatically show a specific page when a RibbonControl is minimized
+
+
+<p>This example demonstrates how to show a specific tab page of a minimized RibbonControl. This works in the same way as when an end-user clicks a ribbon's tab header via the mouse.</p>
+
+<br/>
+
+
---
+++ VB/WindowsFormsApplication4/Form1.Designer.vb
@@ -0,0 +1,144 @@
+﻿Imports Microsoft.VisualBasic
+Imports System
+Namespace WindowsFormsApplication4
+	Partial Public Class Form1
+		''' <summary>
+		''' Required designer variable.
+		''' </summary>
+		Private components As System.ComponentModel.IContainer = Nothing
+
+		''' <summary>
+		''' Clean up any resources being used.
+		''' </summary>
+		''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+			If disposing AndAlso (components IsNot Nothing) Then
+				components.Dispose()
+			End If
+			MyBase.Dispose(disposing)
+		End Sub
+
+		#Region "Windows Form Designer generated code"
+
+		''' <summary>
+		''' Required method for Designer support - do not modify
+		''' the contents of this method with the code editor.
+		''' </summary>
+		Private Sub InitializeComponent()
+			Me.ribbonControl1 = New DevExpress.XtraBars.Ribbon.RibbonControl()
+			Me.ribbonPage1 = New DevExpress.XtraBars.Ribbon.RibbonPage()
+			Me.ribbonPageGroup1 = New DevExpress.XtraBars.Ribbon.RibbonPageGroup()
+			Me.ribbonPageCategory1 = New DevExpress.XtraBars.Ribbon.RibbonPageCategory()
+			Me.ribbonPage2 = New DevExpress.XtraBars.Ribbon.RibbonPage()
+			Me.ribbonPage3 = New DevExpress.XtraBars.Ribbon.RibbonPage()
+			Me.ribbonPage4 = New DevExpress.XtraBars.Ribbon.RibbonPage()
+			Me.ribbonPageGroup2 = New DevExpress.XtraBars.Ribbon.RibbonPageGroup()
+			Me.barButtonItem1 = New DevExpress.XtraBars.BarButtonItem()
+			Me.barButtonItem2 = New DevExpress.XtraBars.BarButtonItem()
+			Me.simpleButton1 = New DevExpress.XtraEditors.SimpleButton()
+			Me.SuspendLayout()
+			' 
+			' ribbonControl1
+			' 
+			Me.ribbonControl1.ApplicationIcon = Nothing
+			Me.ribbonControl1.Items.AddRange(New DevExpress.XtraBars.BarItem() { Me.barButtonItem1, Me.barButtonItem2})
+			Me.ribbonControl1.Location = New System.Drawing.Point(0, 0)
+			Me.ribbonControl1.MaxItemId = 2
+			Me.ribbonControl1.Name = "ribbonControl1"
+			Me.ribbonControl1.PageCategories.AddRange(New DevExpress.XtraBars.Ribbon.RibbonPageCategory() { Me.ribbonPageCategory1})
+			Me.ribbonControl1.Pages.AddRange(New DevExpress.XtraBars.Ribbon.RibbonPage() { Me.ribbonPage1})
+			Me.ribbonControl1.SelectedPage = Me.ribbonPage1
+			Me.ribbonControl1.Size = New System.Drawing.Size(639, 141)
+			' 
+			' ribbonPage1
+			' 
+			Me.ribbonPage1.Groups.AddRange(New DevExpress.XtraBars.Ribbon.RibbonPageGroup() { Me.ribbonPageGroup1})
+			Me.ribbonPage1.Name = "ribbonPage1"
+			Me.ribbonPage1.Text = "ribbonPage1"
+			' 
+			' ribbonPageGroup1
+			' 
+			Me.ribbonPageGroup1.ItemLinks.Add(Me.barButtonItem2)
+			Me.ribbonPageGroup1.Name = "ribbonPageGroup1"
+			Me.ribbonPageGroup1.Text = "ribbonPageGroup1"
+			' 
+			' ribbonPageCategory1
+			' 
+			Me.ribbonPageCategory1.Name = "ribbonPageCategory1"
+			Me.ribbonPageCategory1.Pages.AddRange(New DevExpress.XtraBars.Ribbon.RibbonPage() { Me.ribbonPage2, Me.ribbonPage3, Me.ribbonPage4})
+			Me.ribbonPageCategory1.Text = "ribbonPageCategory1"
+			' 
+			' ribbonPage2
+			' 
+			Me.ribbonPage2.Groups.AddRange(New DevExpress.XtraBars.Ribbon.RibbonPageGroup() { Me.ribbonPageGroup2})
+			Me.ribbonPage2.Name = "ribbonPage2"
+			Me.ribbonPage2.Text = "ribbonPage2"
+			' 
+			' ribbonPage3
+			' 
+			Me.ribbonPage3.Name = "ribbonPage3"
+			Me.ribbonPage3.Text = "ribbonPage3"
+			' 
+			' ribbonPage4
+			' 
+			Me.ribbonPage4.Name = "ribbonPage4"
+			Me.ribbonPage4.Text = "ribbonPage4"
+			' 
+			' ribbonPageGroup2
+			' 
+			Me.ribbonPageGroup2.ItemLinks.Add(Me.barButtonItem1)
+			Me.ribbonPageGroup2.Name = "ribbonPageGroup2"
+			Me.ribbonPageGroup2.Text = "ribbonPageGroup2"
+			' 
+			' barButtonItem1
+			' 
+			Me.barButtonItem1.Caption = "barButtonItem1"
+			Me.barButtonItem1.Id = 0
+			Me.barButtonItem1.Name = "barButtonItem1"
+			' 
+			' barButtonItem2
+			' 
+			Me.barButtonItem2.Caption = "barButtonItem2"
+			Me.barButtonItem2.Id = 1
+			Me.barButtonItem2.Name = "barButtonItem2"
+'			Me.barButtonItem2.ItemClick += New DevExpress.XtraBars.ItemClickEventHandler(Me.barButtonItem2_ItemClick);
+			' 
+			' simpleButton1
+			' 
+			Me.simpleButton1.Location = New System.Drawing.Point(320, 390)
+			Me.simpleButton1.Name = "simpleButton1"
+			Me.simpleButton1.Size = New System.Drawing.Size(168, 42)
+			Me.simpleButton1.TabIndex = 1
+			Me.simpleButton1.Text = "simpleButton1"
+'			Me.simpleButton1.Click += New System.EventHandler(Me.simpleButton1_Click);
+			' 
+			' Form1
+			' 
+			Me.AutoScaleDimensions = New System.Drawing.SizeF(6F, 13F)
+			Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+			Me.ClientSize = New System.Drawing.Size(639, 521)
+			Me.Controls.Add(Me.simpleButton1)
+			Me.Controls.Add(Me.ribbonControl1)
+			Me.Name = "Form1"
+			Me.Text = "Form1"
+'			Me.Load += New System.EventHandler(Me.Form1_Load);
+			Me.ResumeLayout(False)
+
+		End Sub
+
+		#End Region
+
+		Private ribbonControl1 As DevExpress.XtraBars.Ribbon.RibbonControl
+		Private ribbonPage1 As DevExpress.XtraBars.Ribbon.RibbonPage
+		Private ribbonPageGroup1 As DevExpress.XtraBars.Ribbon.RibbonPageGroup
+		Private ribbonPageCategory1 As DevExpress.XtraBars.Ribbon.RibbonPageCategory
+		Private ribbonPage2 As DevExpress.XtraBars.Ribbon.RibbonPage
+		Private ribbonPage3 As DevExpress.XtraBars.Ribbon.RibbonPage
+		Private ribbonPage4 As DevExpress.XtraBars.Ribbon.RibbonPage
+		Private barButtonItem1 As DevExpress.XtraBars.BarButtonItem
+		Private WithEvents barButtonItem2 As DevExpress.XtraBars.BarButtonItem
+		Private ribbonPageGroup2 As DevExpress.XtraBars.Ribbon.RibbonPageGroup
+		Private WithEvents simpleButton1 As DevExpress.XtraEditors.SimpleButton
+	End Class
+End Namespace
+
